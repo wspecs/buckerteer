1f9b1b415a76d368cd8e3d7303cbd08e76add254
Wendly Saintil <wendly@wendly-macbookpro2.roam.corp.google.com>
2019-06-04 13:19:46 -0700

    remove docker rmi command

--- lib/utils.ts
+++ lib/utils.ts
@@ -69,9 +69,9 @@ export function buildNodeProject() {
 
 export function buildDocker(imageName: string) {
   shell.exec(`sudo docker build -t ${imageName} .`);
-  shell.exec(
-    `docker images | grep none | awk '{ print $3; }' | xargs docker rmi`
-  );
+  //   shell.exec(
+  //     `docker images | grep none | awk '{ print $3; }' | xargs docker rmi`
+  //   );
 }
 
 export function createDomainFolder(domain: string) {
